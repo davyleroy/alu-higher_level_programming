bdaaf543722a3e590136856e7d3ff1795d23e4af
davyleroy <d.nkurunziz@alustudent.com>
2023-02-07 12:11:49 -0800

    Melisa

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-if number > 0:
-    digit = number % 10
-else:
-    digit = ((number * -1) % 10) * -1
-        print("Last digit of {:d} is {:d}".format(number, digit), end=" ")
-if digit == 0:
-        print("and is 0")
-elif number < 0 or digit < 6:
-        print("and is less than 6 and not 0")
-else:
-        print("and is greater than 5")
+for letter in range(97, 123):
+    if letter == 101 or letter == 113:
+        continue
+    else:
+        print("{}".format(chr(letter)), end=""
