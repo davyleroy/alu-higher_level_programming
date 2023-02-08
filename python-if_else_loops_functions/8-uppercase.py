39b780710d82ce42f5aea24ce8f62dbc552a7897
davyleroy <d.nkurunziz@alustudent.com>
2023-02-08 06:06:13 -0800

    CHELSEA

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,10 @@
-io:wq
-
+#!/usr/bin/python3
+def uppercase(str):
+    new = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = ord(char) - 32
+            new += chr(char)
+        else:
+            new += char
+    print("{}".format(new))
