17aa885cb8d0e0fbe6be4089ee35ce585cb41a06
davyleroy <d.nkurunziz@alustudent.com>
2023-02-08 06:57:38 -0800

    Die masternn , die besten

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2)
+def remove_char_at(str, n):
+    i = 0
+    new = ""
+for char in str:
+    if i != n:
+        new += char
+        i += 1
+    return (new)
