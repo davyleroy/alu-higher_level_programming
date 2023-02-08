2753b8e411321d2713cb888a77c3c48b1a897230
davyleroy <d.nkurunziz@alustudent.com>
2023-02-08 05:59:38 -0800

    soweto

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
- if ord('a') <= ord(c) <= ord('z'):
-    return True
- else:
-    return False
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
