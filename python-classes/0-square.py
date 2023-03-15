92eae1142608e389d14a56feb78c492281f1ba03
davyleroy <d.nkurunziz@alustudent.com>
2023-03-15 01:59:41 -0700

    One_Love

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square """
+
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification)"""
+    def __init__(self, size):
+        self.__size = size
