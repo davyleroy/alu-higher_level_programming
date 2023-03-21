396813ecfb3cb96b0b4efddf7f9ff0d55d9f45fe
davyleroy <d.nkurunziz@alustudent.com>
2023-03-21 06:37:10 -0700

    One_Love

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+""" Program that validate that object is exact the same """
+
+
 def is_same_class(obj, a_class):
-    """ Function that returns True/False if obj is a type of a_class
-    Args:
-        obj: object
-        a_class: class type
-    Returns:
-        True if type of obj is a_class
-        False, otherwise
-    """
-    return type(obj) is a_class 
+    """ function that returns True if the object is exactly an
+    instance of the specified class ; otherwise False. """
+    if (type(obj) == a_class):
+        return True
+    return False
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+""" Program that checks if a class is the same
+object os inherit from """
+
+
 def is_kind_of_class(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
-    Args:
-        obj: object
-        a_class: class type
-    Returns:
-        True if obj is an instance of a_class
-        False, otherwise
-    """
-    return isinstance(obj, a_class)
+    """  function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False. """
+    return (isinstance(obj, a_class))
