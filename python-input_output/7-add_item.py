4ede5463bef4d4f42951fe60412b2eb4d191dec2
davyleroy <d.nkurunziz@alustudent.com>
2023-03-23 13:02:10 -0700

    One_Love

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""script that adds all arguments to a Python list."""
+
 import sys
 
 if __name__ == "__main__":
