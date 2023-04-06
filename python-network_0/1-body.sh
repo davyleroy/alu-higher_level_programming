22a02d191f8f7dc1211a9a8669cefb3225635cef
davyleroy <d.nkurunziz@alustudent.com>
2023-04-06 16:31:51 +0000

    PROGRAMING_IS_COOL

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,2 +1,3 @@
-sh script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+#!/bin/bash
+#Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sfL "$1" -X GET
